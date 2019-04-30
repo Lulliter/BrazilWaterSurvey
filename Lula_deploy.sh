57481fc601e32827f9ae7db5142c63489c9c2a51
Lulliter <lmm76@georgetown.edu>
2019-04-30 18:23:28 -0400

    rebuilt on Tue Apr 30 18:23:25 EDT 2019

--- Lula_deploy.sh
+++ Lula_deploy.sh
@@ -1,7 +1,21 @@
-	# Go To (git) ./sub-folder (one below the Rproj wdir) 
-	cd ./BrazilWaterSurvey 
-	ls
-	git remote show origin 
+#==================================RESOURCES ON RMARKDOWN_SITE=====================================#
+#   https://bookdown.org/yihui/rmarkdown/rmarkdown-site.html
+#   https://rmarkdown.rstudio.com/lesson-13.html
+#==================================================================================================#
+
+
+
+#=========================================== (start) ================================================#
+#================================== Invoke build_site.R FROM SHELL ================================#
+# ======= ./BrazilWaterSurvey/build_site.R DOES THE FOLLWOING: 
+# ============== 1) list which files will be removed (all ".html(s)" + "site_libs/");
+# ============== 2) actually remove the above files;
+# ============== 3) Render again the ./BrazilWaterSurvey/ public site. 
+cd ./BrazilWaterSurvey # (build_site.R thinks it is in ./)
+pwd
+Rscript build_site.R 
+#=========================================== (end) ================================================#
+
 
 # check status
 git status
@@ -19,7 +33,7 @@ fi
 git commit -m "$msg"
 
 		# or git commit -m "free text "
-		git commit -m "after formatting following Andrew Heiss"
+		git commit -m "massive changes copying MX"
 		
 # Push source and build repos.
 git push origin master
