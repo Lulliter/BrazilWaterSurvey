7443994f4911ac4fb44842c5b0808144bfc0cfb5
Lulliter <lmm76@georgetown.edu>
2019-05-08 16:42:21 -0400

    #css: html/fixes.css

--- Lula_deploy.sh
+++ Lula_deploy.sh
@@ -37,7 +37,7 @@ fi
 git commit -m "$msg"
 
 		# or git commit -m "free text "
-		git commit -m "massive changes copying MX"
+		git commit -m "date in YAMS like AH"
 		
 # Push source and build repos.
 git push origin master
